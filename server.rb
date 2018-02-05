9a6c12b307e534757599ec158325b76f7bbfc7ff
Mark Stahl <markstahl@Marks-MacBook-Pro.local>
2018-02-05 18:59:05 -0500

    First commit

---
+++ server.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+require "pry" if development? || test?
+require "sinatra/reloader" if development?
+
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+get '/' do
+  @ingredients = File.readlines('ingredients.txt')
+  erb :index
+end
+
+post '/ingredients' do
+  ingredient = params['ingredient']
+
+  File.open('ingredients.txt', 'a') do |file|
+    file.puts(ingredient)
+    binding.pry
+  end
+
+  redirect '/'
+end
